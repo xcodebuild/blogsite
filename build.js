9ba9afcd0d4e9dca9c357070ca241f9a6a2a1c08
xcodebuild <me@xcodebuild.com>
2020-05-26 23:29:07 +0800

    use jsdeliver

---
+++ build.js
@@ -0,0 +1,19 @@
+var Hexo = require('hexo');
+
+var hexo = new Hexo(process.cwd(), {});
+
+hexo.init().then(function () {
+
+    hexo.extend.filter.register('after_post_render', function(data){
+      data.content = data.content.replace(/\"(\/images\/.*)\"/g, '"https://cdn.jsdelivr.net/gh/xcodebuild/xcodebuild-blog@master/source/$1"');
+      data.content = data.content.replace(/\"(\/css\/.*)\"/g, '"https://cdn.jsdelivr.net/gh/xcodebuild/xcodebuild-blog@master/source/$1"');
+      data.content = data.content.replace(/\"(\/lib\/.*)\"/g, '"https://cdn.jsdelivr.net/gh/xcodebuild/xcodebuild-blog@master/source/$1"');
+      return data;
+    });
+
+    hexo.call('generate', {}).then(function(){
+        return hexo.exit();
+    }).catch(function(err){
+        return hexo.exit(err);
+    });
+});
